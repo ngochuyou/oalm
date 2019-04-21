a45b458bd6e2f34472cffe712b87bbd3e9e9deb3
ngochuy.ou@gmail.com <ngochuy.ou@gmail.com>
2019-04-22 02:54:39 +0700

    oalm v1.0.0

--- client/src/keys.js
+++ client/src/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	backendURI: 'http://localhost:3000'
+	// backendURI: 'http://localhost:3000'
+	backendURI: 'http://oalm.herokuapp.com'
 }
--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = cors = (req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+	res.setHeader('Access-Control-Allow-Origin', 'http://oalm.herokuapp.com');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type, Accept, x-auth-token');
 	res.setHeader('Access-Control-Allow-Credentials', true);
